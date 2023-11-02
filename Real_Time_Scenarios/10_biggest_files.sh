f52d23e4f2b4704b0292135cb16e376530ee3fa9
Amey Patki <53407006+amey-patki@users.noreply.github.com>
2023-11-02 16:21:02 +0000

    Commit 1

---
+++ Real_Time_Scenarios/10_biggest_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define the directory where you want to start searching
+search_directory="/"
+
+# Define the output file name
+output_file="largest_files.txt"
+
+# Use the find and du commands to find the largest files, sort them, and save the result to the output file
+find "$search_directory" -type f -path "/proc" -prune -o -exec du -h {} + | sort -rh | head -n 10 > "$output_file"
+
+echo "The 10 largest files have been saved to $output_file"
