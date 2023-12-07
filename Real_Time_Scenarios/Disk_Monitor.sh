278fa8b9cfc588560dc013459f6ab157c3eb2555
amey-patki <patkiamey09@gmail.com>
2023-12-07 10:44:44 +0530

    Feat : Disk Monitor

---
+++ Real_Time_Scenarios/Disk_Monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the threshold for disk usage in percentage
+threshold=90
+
+# Get the current disk usage
+disk_usage=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
+
+# Check if disk usage exceeds the threshold
+if [ "$disk_usage" -gt "$threshold" ]; then
+    echo "Warning: Disk usage is high! Current usage: $disk_usage%"
+    # Add actions to take when disk usage is high, such as sending an alert
+fi
