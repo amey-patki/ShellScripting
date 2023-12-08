1b3e2fc745a80a657b37da94b0b4b8c86a941d20
amey-patki <patkiamey09@gmail.com>
2023-12-08 20:32:44 +0530

    removed comment

--- Real_Time_Scenarios/Disk_Monitor.sh
+++ Real_Time_Scenarios/Disk_Monitor.sh
@@ -9,5 +9,5 @@ disk_usage=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
 # Check if disk usage exceeds the threshold
 if [ "$disk_usage" -gt "$threshold" ]; then
     echo "Warning: Disk usage is high! Current usage: $disk_usage%"
-    # Add actions to take when disk usage is high, such as sending an alert
+   
 fi
